145285e8c1622e0e8ad1323720dab960611db319
JacksonTeh <jackson_dmc69@hotmail.com>
2014-12-24 00:57:07 +0800

    Test Travis

--- test/test_DigitalEventSimulator.c
+++ test/test_DigitalEventSimulator.c
@@ -169,7 +169,7 @@ void test_eventSimulator_given_node_contain_pipe_module_with_pipe_data_should_re
     // TEST_ASSERT_EQUAL(ONE_NANO_SEC + AND_PROPAGATION_DELAY, result->time);
 
     // destroyModuleAndPin(moduleAndPin);
-    destroyPipeData(pipe);
-    destroyModule(AND);
-    destroyEventNode(eventRoot);
+    // destroyPipeData(pipe);
+    // destroyModule(AND);
+    // destroyEventNode(eventRoot);
 }
